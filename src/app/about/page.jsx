dce7810a03037ba59475bf2854cf81b9de92811c
AM <malerischam@yahoo.com>
2024-11-01 01:39:30 +0400

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Deleniti velit quam voluptas in ipsum explicabo ab tempora omnis, incidunt fugit voluptatum at nostrum error hic beatae ipsa temporibus voluptates laborum.
+        </p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, et, iure sed perspiciatis commodi eligendi reprehenderit fuga ab numquam quas quisquam illum repellat quo doloribus blanditiis tenetur dignissimos animi ipsum!Lorem sicing elit. Nihil, et, iure sed perspiciatis commodi eligendi reprehenderit fuga
+           ab numquam quas quisquam illum repellat quo </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae magni blanditiis illum quas natus fuga dolores vel. Quia velit aliquid excepturi eius optio quod odio quis distinctio? Ea, cupiditate mollitia! Repudiandae magni blanditiis illum quas natus </p>
+        
+    </div>
+  )
+}
